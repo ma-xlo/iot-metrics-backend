453fe9884bcab6503240da1393a806b3e52c59f8
Marcelo Correa <macorrea-@hotmail.com>
2024-09-05 18:12:44 -0300

    fix: adjust device tags urls

--- apps/devices/urls.py
+++ apps/devices/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('devices/tags/', views.list_device_tags, name='list-device-tags'),
-    path('devices/<int:device_id>/tags/', views.detail_device_tags, name='detail-device-tags'),
-    path('devices/online/', views.list_online_devices, name='list-online-devices'),
+    # Devices
+    # path('devices/', views.list_devices, name='detail-device-tags'),
+    # path('devices/online/', views.list_online_devices, name='list-online-devices'),
+    
+    # Device Tags
+    path('devices/<str:device_id>/tags/', views.list_device_tags, name='list-device-tags'),
+    path('devices/<str:device_id>/tags/<int:tag_id>', views.detail_device_tags, name='detail-device-tags'),
 ]
--- apps/devices/views.py
+++ apps/devices/views.py
@@ -10,10 +10,9 @@ from datetime import timedelta
 from django.db import connection
 
 @api_view(['GET', 'POST'])
-def list_device_tags(request):
+def list_device_tags(request, device_id):
   if request.method == 'POST':
     tag_name = request.data.get('tag')
-    device_id = request.data.get('device_id')
 
     new_tag, created = Tags.objects.get_or_create(name=tag_name)
     device_tag, created = TagDevice.objects.get_or_create(tag_id=new_tag.id, device_id=device_id)
@@ -22,33 +21,28 @@ def list_device_tags(request):
     return Response(serializer.data, status=status.HTTP_200_OK)
    
   if request.method == 'GET':
-    with connection.cursor() as cursor:
-      cursor.execute('''
-          SELECT td.id, td.device_id, t.name as tag_name
-          FROM tag_device td
-          INNER JOIN tags t ON td.tag_id = t.id
-      ''')
-      results = cursor.fetchall()
-
-    response_data = [{"id": row[0], "device_id": row[1], "tag_name": row[2]} for row in results]
-    return Response(response_data, status=status.HTTP_200_OK)
-  return Response({"message": "Method not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-@api_view(['GET', 'POST'])
-def detail_device_tags(request, device_id):
-  
-  if request.method == 'GET':
     device_tag = TagDevice.objects.filter(device_id=device_id)
+    if not device_tag.exists():
+      return Response([], status=status.HTTP_404_NOT_FOUND)
+    
     serializer = TagDeviceSerializer(device_tag, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
   return Response({"message": "Method not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-@api_view(['GET'])
-def list_online_devices(request):
-    latest_entries = (
-        Metrics.objects.all() 
-        .values('deviceid')
-        .annotate(latest_timestamp=Max('timestamp'))
-    )
-    result = [{"device_id": entry['deviceid'], "timestamp": entry['latest_timestamp']} for entry in latest_entries]
-    return Response(result, status=status.HTTP_200_OK)  
+@api_view(['GET', 'DELETE'])
+def detail_device_tags(request, device_id, tag_id):
+  if request.method == 'DELETE':
+    device_tag = TagDevice.objects.filter(device_id=device_id, tag_id=tag_id)
+    device_tag.delete()
+    return Response({"message": "ok"}, status=status.HTTP_200_OK)  
+  return Response({"message": "Method not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+# @api_view(['GET'])
+# def list_online_devices(request):
+#     latest_entries = (
+#         Metrics.objects.all() 
+#         .values('deviceid')
+#         .annotate(latest_timestamp=Max('timestamp'))
+#     )
+#     result = [{"device_id": entry['deviceid'], "timestamp": entry['latest_timestamp']} for entry in latest_entries]
+#     return Response(result, status=status.HTTP_200_OK)  
