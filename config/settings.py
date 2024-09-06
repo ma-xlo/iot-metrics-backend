443b91f6f709890d5d56a04619ea9ec02446f320
macorrea <macorrea-@hotmail.com>
2024-09-06 20:03:49 -0300

    Deploy

--- config/settings.py
+++ config/settings.py
@@ -63,12 +63,10 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-
 ]
 
-
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    'https://iot-metrics-front.vercel.app',
 ]
 
 ROOT_URLCONF = "config.urls"
