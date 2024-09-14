d6182a588c9aabc0825e1b126ad364f4d373deff
macorrea <macorrea-@hotmail.com>
2024-09-14 15:22:59 -0300

    feat: improve query for metrics

--- apps/core/helpers.py
+++ apps/core/helpers.py
@@ -1,13 +1,18 @@
 from datetime import datetime
+from dateutil import parser
 
 ONLINE_TIME_THRESHOLD = 300
 
-def is_device_online (device_data):
-  timestamp_str = device_data[len(device_data) - 1]['timestamp']
-  last_timestamp = datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%SZ")
-  current_time = datetime.now()
-  
-  if ((current_time.timestamp() - last_timestamp.timestamp()) > ONLINE_TIME_THRESHOLD):
-    return False;
-  
-  return True;
+def is_device_online(device_data):
+    # Get the timestamp string from the last entry
+    timestamp_str = device_data[-1]['timestamp']
+    
+    # Parse the timestamp string into a datetime object using dateutil.parser
+    last_timestamp = parser.parse(timestamp_str)
+    current_time = datetime.now()
+    
+    # Calculate the time difference in seconds
+    if (current_time.timestamp() - last_timestamp.timestamp()) > ONLINE_TIME_THRESHOLD:
+        return False
+
+    return True
--- apps/metrics/views.py
+++ apps/metrics/views.py
@@ -2,45 +2,39 @@ from collections import defaultdict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from django.db.models import Max
+from django.db.models import OuterRef, Subquery
 from apps.core.models import Metrics, TagDevice
 from .serializers import MetricsSerializer
 from django.utils import timezone
 from datetime import timedelta
 from apps.core.helpers import is_device_online
+from django.db import connection
+import json
 
 @api_view(['GET'])
 def list_metrics(request):
-    time_threshold = timezone.now() - timedelta(minutes=60)
-
     if request.method == 'GET':
-        # metrics = Metrics.objects.all()
-        metrics = Metrics.objects.filter(timestamp__gte=time_threshold)
-        serializer = MetricsSerializer(metrics, many=True)
-        serialized_data = serializer.data
-
-        data_by_device = defaultdict(list)
-        for item in serialized_data:
-            device_id = item.pop('deviceid')
-            data_by_device[device_id].append(item)
-
-        device_ids = list(data_by_device.keys())
-        tag_devices = TagDevice.objects.filter(device_id__in=device_ids)
-
-        tags_by_device = defaultdict(list)
-        for tag_device in tag_devices:
-            tags_by_device[tag_device.device_id].append(tag_device.tag_id)
-
-        result = [
-            {
-                "id": device_id,
-                "data": data,
-                "online": is_device_online(data),
-                "tags": tags_by_device[device_id]  
-            }
-            for device_id, data in data_by_device.items()
-        ]
-
-        return Response(result, status=status.HTTP_200_OK)
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute('CALL GetMetrics()')
+                results = cursor.fetchall()
+                
+                # Process the results to convert to JSON
+                processed_results = []
+                for row in results:
+                    device_id, data_json, tags_json = row
+                    data = json.loads(data_json)  # Convert JSON string to Python list
+                    tags = json.loads(tags_json)  # Convert JSON string to Python list
+                    processed_results.append({
+                        "id": device_id,
+                        "data": data,
+                        "online": is_device_online(data),
+                        "tags": tags
+                    })
+                
+                return Response(processed_results, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
     return Response({"message": "Method not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
